71a7ecdff235c15b64b8aeee0d4085ab5ba5718f
random-entity <randomentity.official@gmail.com>
2024-05-09 16:58:31 +0900

    Fix a very minor mistake

--- lib/enums.hpp
+++ lib/enums.hpp
@@ -15,16 +15,22 @@ inline std::ostream& operator<<(std::ostream& os,
   switch (resolution) {
     case moteus::Resolution::kInt8:
       os << "kInt8";
+      break;
     case moteus::Resolution::kInt16:
       os << "kInt16";
+      break;
     case moteus::Resolution::kInt32:
       os << "kInt32";
+      break;
     case moteus::Resolution::kFloat:
       os << "kFloat";
+      break;
     case moteus::Resolution::kIgnore:
       os << "kIgnore";
+      break;
     default:
       os << "<Unknown Resolution>";
+      break;
   }
   return os;
 }
