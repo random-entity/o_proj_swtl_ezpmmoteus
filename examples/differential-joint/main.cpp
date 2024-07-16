ab82e439f32fc946e012d5325077d40c7696590a
random-entity <randomentity.official@gmail.com>
2024-07-16 11:17:00 +0900

    Let differential joint script control gf3 left arm

--- examples/differential-joint/main.cpp
+++ examples/differential-joint/main.cpp
@@ -3,7 +3,7 @@
 using namespace som;
 
 int main() {
-  std::string host_dest = "192.168.0.6";
+  std::string host_dest = "192.168.0.25";
   // std::string host_src = "0.0.0.0";
   const int udp_receive_port = 5555;
   const int udp_send_port = 8888;
@@ -11,14 +11,9 @@ int main() {
   ///////////////////////////////////////////////////////////////
   /// Initialize the ServoSystem, and check if the Servos of
   /// right IDs are initialized.
-  DifferentialJointUdpServoSystem servo_system{host_dest,
-                                               udp_receive_port, udp_send_port};
+  DifferentialJointUdpServoSystem servo_system{host_dest, udp_receive_port,
+                                               udp_send_port};
   const auto& ids = servo_system.GetIds();
-  if (ids != std::set{4, 5}) {
-    std::cout << "Initialization failed." << std::endl;
-    return 0;
-  }
-  sleep(1);
 
   ///////////////////////////////////////////////////////////////
   /// Suspend main thread termination while listening to
