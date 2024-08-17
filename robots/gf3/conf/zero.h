6353b1c276b37e102709681e041e98d3e81decb3
random-entity <randomentityemail@gmail.com>
2024-08-18 05:14:59 +0900

    Fix mistakes: Let Zero script to test MoveTo output 0; Delete infinite loop in Executer::Run

--- robots/gf3/conf/zero.h
+++ robots/gf3/conf/zero.h
@@ -2,11 +2,12 @@
 
 #pragma once
 
-#include "../servo_units/gf3.h"
+#include "../executer.h"
 
 namespace gf3::utils {
 
 void Zero(GF3& gf3) {
+  // Zero.
   for (const auto& s : gf3.servos_set_) {
     s->SetStop();
     usleep(0.1e6);
@@ -16,7 +17,31 @@ void Zero(GF3& gf3) {
     usleep(0.1e6);
   }
 
-  for (const auto& s : gf3.servos_set_) s->SetPosition(global_pm_cmd_template);
+  // Test.
+  std::vector<CanFdFrame> command_frames;
+  for (const auto& j : gf3.saj_set_) {
+    j->cmd_.mode = SingleAxisJoint::Command::Mode::MoveTo;
+    j->cmd_.move_to.target_out = 0.0;
+    j->cmd_.move_to.max_trq = 64.0;
+    j->cmd_.move_to.max_vel = 64.0;
+    j->cmd_.move_to.max_acc = 32.0;
+    const auto saj_frames = SingleAxisJointFrameMakers::methods.at(
+        SingleAxisJoint::Command::Mode::MoveTo)(j);
+    utils::Merge(command_frames, saj_frames);
+  }
+  for (const auto& j : gf3.dj_set_) {
+    j->cmd_.mode = DifferentialJoint::Command::Mode::MoveTo;
+    j->cmd_.move_to.target_avg = 0.0;
+    j->cmd_.move_to.target_dif = 0.0;
+    j->cmd_.move_to.max_trq = 64.0;
+    j->cmd_.move_to.max_vel = 64.0;
+    j->cmd_.move_to.max_acc = 32.0;
+    const auto dj_frames = DifferentialJointFrameMakers::methods.at(
+        DifferentialJoint::Command::Mode::MoveTo)(j);
+    utils::Merge(command_frames, dj_frames);
+  }
+  global_transport->BlockingCycle(&command_frames[0], command_frames.size(),
+                                  nullptr);
 }
 
 }  // namespace gf3::utils
--- robots/gf3/executer.h
+++ robots/gf3/executer.h
@@ -11,58 +11,50 @@ class Executer {
       : gf3_{gf3}, beat_{interval} {}
 
   void Run() {
-    while (1) {
-      if (beat_.Hit()) {
-        // Receive Commands.
+    // Query and distribute Replies.
+    std::vector<CanFdFrame> query_frames;
+    std::vector<CanFdFrame> reply_frames;
+    for (const auto& s : gf3_.servos_set_) {
+      query_frames.push_back(s->MakeQuery());
+    }
+    global_transport->BlockingCycle(&query_frames[0], query_frames.size(),
+                                    &reply_frames);
+    for (const auto& frame : reply_frames) {
+      const auto& id = static_cast<int>(frame.source);
+      const auto maybe_servo = utils::SafeAt(gf3_.servos_map_, id);
+      if (!maybe_servo) continue;
+      maybe_servo.value()->SetReply(Query::Parse(frame.data, frame.size));
+    }
 
-        // Query and distribute Replies.
-        std::vector<CanFdFrame> query_frames;
-        std::vector<CanFdFrame> reply_frames;
-        for (const auto& s : gf3_.servos_set_) {
-          query_frames.push_back(s->MakeQuery());
-        }
-        global_transport->BlockingCycle(&query_frames[0], query_frames.size(),
-                                        &reply_frames);
-        for (const auto& frame : reply_frames) {
-          const auto& id = static_cast<int>(frame.source);
-          const auto maybe_servo = utils::SafeAt(gf3_.servos_map_, id);
-          if (!maybe_servo) continue;
-          maybe_servo.value()->SetReply(Query::Parse(frame.data, frame.size));
-        }
-
-        // Execute Commands.
-        std::vector<CanFdFrame> command_frames;
-        for (const auto& j : gf3_.saj_set_) {
-          try {
-            const auto saj_frames =
-                SingleAxisJointFrameMakers::methods.at(j->cmd_.mode)(j);
-            utils::Merge(command_frames, saj_frames);
-          } catch (const std::out_of_range& e) {
-            std::cout << "Check if SingleAxisJointFrameMakers::methods "
-                         "has all SingleAxisJoint::Command::Mode registered."
-                      << std::endl;
-            std::cerr << e.what() << '\n';
-          }
-        }
-        for (const auto& j : gf3_.dj_set_) {
-          try {
-            const auto dj_frames =
-                DifferentialJointFrameMakers::methods.at(j->cmd_.mode)(j);
-            utils::Merge(command_frames, dj_frames);
-
-          } catch (const std::out_of_range& e) {
-            std::cout << "Check if DifferentialJointFrameMakers::methods "
-                         "has all DifferentialJoint::Command::Mode registered."
-                      << std::endl;
-            std::cerr << e.what() << '\n';
-          }
-        }
-        global_transport->BlockingCycle(&command_frames[0],
-                                        command_frames.size(), nullptr);
-
-        // Send Replies.
+    // Execute Commands.
+    std::vector<CanFdFrame> command_frames;
+    for (const auto& j : gf3_.saj_set_) {
+      try {
+        const auto saj_frames =
+            SingleAxisJointFrameMakers::methods.at(j->cmd_.mode)(j);
+        utils::Merge(command_frames, saj_frames);
+      } catch (const std::out_of_range& e) {
+        std::cout << "Check if SingleAxisJointFrameMakers::methods "
+                     "has all SingleAxisJoint::Command::Mode registered."
+                  << std::endl;
+        std::cerr << e.what() << '\n';
       }
     }
+    for (const auto& j : gf3_.dj_set_) {
+      try {
+        const auto dj_frames =
+            DifferentialJointFrameMakers::methods.at(j->cmd_.mode)(j);
+        utils::Merge(command_frames, dj_frames);
+
+      } catch (const std::out_of_range& e) {
+        std::cout << "Check if DifferentialJointFrameMakers::methods "
+                     "has all DifferentialJoint::Command::Mode registered."
+                  << std::endl;
+        std::cerr << e.what() << '\n';
+      }
+    }
+    global_transport->BlockingCycle(&command_frames[0], command_frames.size(),
+                                    nullptr);
   }
 
  private:
