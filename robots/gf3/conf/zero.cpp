fa488953ee4209eb86e2597c4ce4adcdb38365e8
random-entity <randomentityemail@gmail.com>
2024-08-19 23:17:50 +0900

    Move zero script test command to zero.cpp

--- robots/gf3/conf/zero.cpp
+++ robots/gf3/conf/zero.cpp
@@ -5,6 +5,33 @@ using namespace gf3;
 int main() {
   GF3 gf3{};
   utils::Zero(gf3);
+
+  // Test.
+  std::vector<CanFdFrame> command_frames;
+  for (const auto& j : gf3.saj_set_) {
+    j->cmd_.mode = SingleAxisJoint::Command::Mode::MoveTo;
+    j->cmd_.move_to.target_out = 0.0;
+    j->cmd_.move_to.max_trq = 64.0;
+    j->cmd_.move_to.max_vel = 64.0;
+    j->cmd_.move_to.max_acc = 32.0;
+    const auto saj_frames = SingleAxisJointFrameMakers::methods.at(
+        SingleAxisJoint::Command::Mode::MoveTo)(j);
+    utils::Merge(command_frames, saj_frames);
+  }
+  for (const auto& j : gf3.dj_set_) {
+    j->cmd_.mode = DifferentialJoint::Command::Mode::MoveTo;
+    j->cmd_.move_to.target_avg = 0.0;
+    j->cmd_.move_to.target_dif = 0.0;
+    j->cmd_.move_to.max_trq = 64.0;
+    j->cmd_.move_to.max_vel = 64.0;
+    j->cmd_.move_to.max_acc = 32.0;
+    const auto dj_frames = DifferentialJointFrameMakers::methods.at(
+        DifferentialJoint::Command::Mode::MoveTo)(j);
+    utils::Merge(command_frames, dj_frames);
+  }
+  global_transport->BlockingCycle(&command_frames[0], command_frames.size(),
+                                  nullptr);
+
   while (1);
 
   return 0;
--- robots/gf3/conf/zero.h
+++ robots/gf3/conf/zero.h
@@ -16,32 +16,6 @@ void Zero(GF3& gf3) {
     s->DiagnosticCommand("conf write");
     usleep(0.1e6);
   }
-
-  // Test.
-  std::vector<CanFdFrame> command_frames;
-  for (const auto& j : gf3.saj_set_) {
-    j->cmd_.mode = SingleAxisJoint::Command::Mode::MoveTo;
-    j->cmd_.move_to.target_out = 0.0;
-    j->cmd_.move_to.max_trq = 64.0;
-    j->cmd_.move_to.max_vel = 64.0;
-    j->cmd_.move_to.max_acc = 32.0;
-    const auto saj_frames = SingleAxisJointFrameMakers::methods.at(
-        SingleAxisJoint::Command::Mode::MoveTo)(j);
-    utils::Merge(command_frames, saj_frames);
-  }
-  for (const auto& j : gf3.dj_set_) {
-    j->cmd_.mode = DifferentialJoint::Command::Mode::MoveTo;
-    j->cmd_.move_to.target_avg = 0.0;
-    j->cmd_.move_to.target_dif = 0.0;
-    j->cmd_.move_to.max_trq = 64.0;
-    j->cmd_.move_to.max_vel = 64.0;
-    j->cmd_.move_to.max_acc = 32.0;
-    const auto dj_frames = DifferentialJointFrameMakers::methods.at(
-        DifferentialJoint::Command::Mode::MoveTo)(j);
-    utils::Merge(command_frames, dj_frames);
-  }
-  global_transport->BlockingCycle(&command_frames[0], command_frames.size(),
-                                  nullptr);
 }
 
 }  // namespace gf3::utils
