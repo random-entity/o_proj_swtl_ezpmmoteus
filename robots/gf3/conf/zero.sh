b06a30ceaec11e47873190c1fae4d4409f62841d
random-entity <randomentityemail@gmail.com>
2024-08-20 18:07:59 +0900

    Let the zeroing script to accept target IDs from command line arguments

--- robots/gf3/conf/zero.sh
+++ robots/gf3/conf/zero.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-for i in {1..14}
+if [ $# -eq 0 ]; then
+  echo "No arguments provided, running for targets 1 to 14."
+  targets=$(seq 1 14)
+else
+  targets="$@"
+fi
+
+for i in $targets
 do
   echo "Running zero.exp with target $i..."
   expect ./zero.exp $i
