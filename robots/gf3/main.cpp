250fe062f6a9ada112bb0d574c674d76a904ea13
random-entity <randomentityemail@gmail.com>
2024-08-24 21:20:28 +0900

    Minor fix: check argc for main arguments

--- robots/gf3/main.cpp
+++ robots/gf3/main.cpp
@@ -9,17 +9,16 @@ int main(int argc, char** argv) {
   GF3 gf3{};
   Executer executer{gf3};
 
-  const std::string udp_cr_host = argv[1] ? argv[1] : "127.0.0.1";
-  const int udp_cr_port = argv[2] ? std::stoi(argv[1]) : 8888;
-  const std::string udp_rs_host = argv[3] ? argv[3] : "127.0.0.1";
-  const int udp_rs_port = argv[4] ? std::stoi(argv[4]) : 5555;
-
+  const std::string udp_cr_host = argc >= 2 ? argv[1] : "127.0.0.1";
+  const int udp_cr_port = argc >= 3 ? std::stoi(argv[1]) : 8888;
   UdpCommandReceiver udp_cr{gf3, udp_cr_host, udp_cr_port};
   if (!udp_cr.Setup()) return 1;
   std::thread udp_cr_thread{[&] {
     while (1) udp_cr.Run();
   }};
 
+  const std::string udp_rs_host = argc >= 4 ? argv[3] : "127.0.0.1";
+  const int udp_rs_port = argc >= 5 ? std::stoi(argv[4]) : 5555;
   UdpReplySender udp_rs{gf3, udp_rs_host, udp_rs_port};
   if (!udp_rs.Setup()) return 1;
 
