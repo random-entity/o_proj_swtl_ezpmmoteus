318eb6d58fd87d531ed1d9e50e0ba9080f4972b8
random-entity <randomentityemail@gmail.com>
2024-08-26 16:31:11 +0900

    Stop PID gain tuning for each joints and unify value to 10,1,0.1; Set min/max output position for right arm

--- conf/conf-bus2_fdcanusb.sh
+++ conf/conf-bus2_fdcanusb.sh
@@ -6,7 +6,6 @@ moteus_tool -t 7,8,9,10,11,12 --write-config common.cfg
 moteus_tool -t 7,12 --write-config saj.cfg
 moteus_tool -t 8,10 --write-config dj_l.cfg
 moteus_tool -t 9,11 --write-config dj_r.cfg
-moteus_tool -t 12 --write-config wrist.cfg
 moteus_tool -t 7 --write-config pid_shoulder_z.cfg
 moteus_tool -t 8,9 --write-config pid_shoulder_xy.cfg
 moteus_tool -t 10,11 --write-config pid_elbow.cfg
