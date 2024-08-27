2c8e7c4013012f09d77f6456e5ca7baddfafe288
random-entity <randomentityemail@gmail.com>
2024-08-27 15:59:16 +0900

    Change pi3hat config

--- src/gf3/globals.h
+++ src/gf3/globals.h
@@ -9,8 +9,9 @@ std::shared_ptr<Transport> transport{[] {
   mjbots::pi3hat::ConfigureRealtime(2);
   mjbots::pi3hat::Pi3HatMoteusFactory::Register();
   return Controller::MakeSingletonTransport(
-      {"--pi3hat-cpu", "3", "--pi3hat-cfg",
-       "1=1,2,3,4,5,6;2=7,8,9,10,11,12;3=13,14", "--pi3hat-disable-aux"});
+      {"--pi3hat-cpu", "1",  //
+       "--pi3hat-cfg", "1=1,2,3,4,5,6;2=7,8,9,10,11,12;3=13,14",
+       "--pi3hat-disable-aux"});
 #else
   return Controller::MakeSingletonTransport({});
 #endif
