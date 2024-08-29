f9de52f137e78c5c35eb5e97fba8b5b0044024e1
random-entity <randomentityemail@gmail.com>
2024-08-29 12:28:22 +0900

    Rename conf script files

--- conf/conf-bus1_fdcanusb.sh
+++ conf/conf-bus1_fdcanusb.sh
@@ -3,9 +3,9 @@
 # Do NOT use this often!
 
 moteus_tool -t 1,2,3,4,5,6 --write-config common.cfg
-moteus_tool -t 1,6 --write-config saj.cfg
-moteus_tool -t 2,4 --write-config dj_l.cfg
-moteus_tool -t 3,5 --write-config dj_r.cfg
+moteus_tool -t 1,6 --write-config sgn_saj.cfg
+moteus_tool -t 2,4 --write-config sgn_dj_l.cfg
+moteus_tool -t 3,5 --write-config sgn_dj_r.cfg
 moteus_tool -t 1 --write-config pid_shoulder_z.cfg
 moteus_tool -t 2,3 --write-config pid_shoulder_xy.cfg
 moteus_tool -t 4,5 --write-config pid_elbow.cfg
--- conf/conf-bus3_fdcanusb.sh
+++ conf/conf-bus3_fdcanusb.sh
@@ -3,7 +3,7 @@
 # Do NOT use this often!
 
 moteus_tool -t 13,14 --write-config common.cfg
-moteus_tool -t 13 --write-config dj_l.cfg
-moteus_tool -t 14 --write-config dj_r.cfg
+moteus_tool -t 13 --write-config sgn_dj_l.cfg
+moteus_tool -t 14 --write-config sgn_dj_r.cfg
 moteus_tool -t 13,14 --write-config pid_neck.cfg
 moteus_tool -t 13,14 --write-config conf-write.cfg
