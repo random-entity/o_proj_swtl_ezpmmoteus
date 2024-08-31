3fdab7b7788550dad12dbd171013f1516a0b856e
random-entity <randomentityemail@gmail.com>
2024-08-31 21:58:34 +0900

    Let DifferentialJoint OutPos Mode use velocity_limit set by Command velocity

--- src/gf3/frame_makers/dj_out_pos.h
+++ src/gf3/frame_makers/dj_out_pos.h
@@ -7,6 +7,8 @@ namespace gf3 {
 std::vector<CanFdFrame> DifferentialJointFrameMakers::OutPos(
     DifferentialJoint* j) {
   auto& cmd = j->cmd_;
+  cmd.vel_dif = std::abs(cmd.vel_dif);
+  cmd.vel_avg = std::abs(cmd.vel_avg);
   auto& rpl = j->rpl_;
 
   const auto target_pos_dif =
@@ -48,20 +50,33 @@ std::vector<CanFdFrame> DifferentialJointFrameMakers::OutPos(
     return {};
   }
 
+  const auto target_vel_dif =
+      cmd.vel_dif * std::clamp(target_delta_pos_dif / cmd.damp_thr, -1.0, 1.0);
+  const auto target_vel_avg =
+      cmd.vel_avg * std::clamp(target_delta_pos_avg / cmd.damp_thr, -1.0, 1.0);
+  const auto target_vel_rotor_l =
+      j->r_avg_ * target_vel_avg + j->r_dif_ * target_vel_dif;
+  const auto target_vel_rotor_r =
+      j->r_avg_ * target_vel_avg - j->r_dif_ * target_vel_dif;
+
   auto pm_cmd = *(j->pm_cmd_template_);
   pm_cmd.velocity = 0.0;
   pm_cmd.maximum_torque = cmd.max_trq;
-  pm_cmd.velocity_limit = cmd.max_vel;
+  // pm_cmd.velocity_limit = cmd.max_vel;
   pm_cmd.accel_limit = cmd.max_acc;
 
   return {j->l_.MakePositionRelativeToRecent([&] {
             auto cmd = pm_cmd;
-            cmd.position = 0.5 * target_delta_pos_rotor_l;
+            cmd.position = target_delta_pos_rotor_l;
+            // cmd.position = 0.04 * target_vel_rotor_l;
+            cmd.velocity_limit = std::abs(target_vel_rotor_l);
             return cmd;
           }()),
           j->r_.MakePositionRelativeToRecent([&] {
             auto cmd = pm_cmd;
-            cmd.position = 0.5 * target_delta_pos_rotor_r;
+            cmd.position = target_delta_pos_rotor_r;
+            // cmd.position = 0.04 * target_vel_rotor_r;
+            cmd.velocity_limit = std::abs(target_vel_rotor_r);
             return cmd;
           }())};
 }
