a0df421ac3618b7eac3c5367a76dae02c15d0da4
random-entity <randomentityemail@gmail.com>
2024-09-05 16:55:10 +0900

    Add while loop to ssh script

--- deploy/ssh-access-rpi.sh
+++ deploy/ssh-access-rpi.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-ssh ubuntu@192.168.0.103
+while true; do
+  echo "Try connecting to RPi..."
+  ssh ubuntu@192.168.0.103
+  if [ $? -ne 0 ]; then
+    echo "Failed. retrying in 5 seconds..."
+    sleep 5
+  else
+    break
+  fi
+done
 
 # Access som-rpi4-2 with this script, then run
 # `./gf3_launch.sh` within the /home/ubuntu/ directory.
